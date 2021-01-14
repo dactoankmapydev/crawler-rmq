ed1faab61ee4e5cf1e8a2652bf6d799d74e3bad9
Nguyễn Đắc Toàn <nguyendactoankma@gmail.com>
2021-01-14 18:59:39 +0700

    Update otx.py

--- otx.py
+++ otx.py
@@ -1,42 +1,82 @@
-import pika, helper
-import requests
+import json
 import math
+from datetime import datetime
 
+import helper
+import pika
+import requests
+import pytz
 
-OTX_API_KEY = '779cc51038ddb07c5f6abe0832fed858a6039b9e8cdb167d3191938c1391dbba'
+OTX_API_KEY = "779cc51038ddb07c5f6abe0832fed858a6039b9e8cdb167d3191938c1391dbba"
 
 headers = {
-    'X-OTX-API-KEY': OTX_API_KEY
+    "X-OTX-API-KEY": OTX_API_KEY
 }
 
 proxies = {
-        'http': 'http://127.0.0.1:3131',
-        'https': 'http://127.0.0.1:3131',
-    }
+    "http": "http://127.0.0.1:3131",
+    "https": "http://127.0.0.1:3131",
+}
+
 
 def send_indicator(channel, message):
-    channel.basic_publish(exchange='',
-        routing_key='ioc_collector',
-        body=message,
-        properties=pika.BasicProperties(delivery_mode = 2))
+    channel.basic_publish(exchange="",
+                          routing_key="ioc_collect_queue",
+                          body=json.dumps(message),
+                          properties=pika.BasicProperties(priority=5))
     print("Sent %r" % message)
 
-def run():
-    connection, channel = helper.setup('ioc_collector')
 
-    results = requests.get('https://otx.alienvault.com/api/v1/pulses/subscribed', headers=headers).json()
-    page = math.ceil(results['count']/50)
-    for page in range (1, page+1):
-        data = requests.get('https://otx.alienvault.com/api/v1/pulses/subscribed?limit=50&page={}'.format(page), headers=headers).json()
-        for item in data['results']:
-            for value in item['indicators']:
-                name_indicator = value['indicator']
-                type_indicator = value['type']
-                created = value['created']
-                send_indicator(channel, name_indicator)
-                send_indicator(channel, type_indicator)
-                send_indicator(channel, created)
+def get_total_page():
+    results = requests.get("https://otx.alienvault.com/api/v1/pulses/subscribed", headers=headers,
+                           proxies=proxies).json()
+    total_page = math.ceil(results["count"] / 50)
+    return total_page
+
+
+def run():
+    connection, channel = helper.setup("ioc_collect_queue")
+    ist = pytz.timezone("Asia/Ho_Chi_Minh")
+    total_page = get_total_page()
+
+    sample = {"sample": ["FileHash-MD5", "FileHash-PEHASH", "FileHash-SHA256", "FileHash-IMPHASH", "FileHash-MD5"]}
+    url = {"url": ["URL", "URI"]}
+    domain = {"domain": ["hostname", "domain"]}
+    ipaddress = {"ipaddress": ["IPv6", "IPv4", "BitcoinAddress"]}
+
+    for page in range(1, total_page + 1):
+        data = requests.get("https://otx.alienvault.com/api/v1/pulses/subscribed?limit=50&page={}".format(page),
+                            headers=headers, proxies=proxies).json()
+        for item in data["results"]:
+            category = item["tags"]
+            for value in item["indicators"]:
+                ioc_id = value["id"]
+                ioc = value["indicator"]
+                created_time = value["created"]
+                ioc_type = value["type"]
+
+                if ioc_type in sample["sample"]:
+                    ioc_type = "sample"
+                if ioc_type in url["url"]:
+                    ioc_type = "url"
+                if ioc_type in domain["domain"]:
+                    ioc_type = "domain"
+                if ioc_type in ipaddress["ipaddress"]:
+                    ioc_type = "ipaddress"
+
+                indicator = {
+                    "ioc_id": ioc_id,
+                    "ioc": ioc,
+                    "ioc_type": ioc_type,
+                    "created_time": created_time,
+                    "crawled_time": datetime.now(ist).strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3],
+                    "source": "otx-alienvault",
+                    "category": category
+                }
+                send_indicator(channel, indicator)
     connection.close()
+
 
 if __name__ == "__main__":
     run()
+
